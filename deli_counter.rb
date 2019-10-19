00f18e589b7ea6e841997dd2c7a0d5dd9b6d6d68
Eric Manning <emanningmusic@yahoo.com>
2019-10-19 14:58:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(deli)
   if deli.empty?
     puts "The line is currently empty."
   else
-    loop other_deli do 
+    loop other_deli and people_in_line do 
       people_in_line +=1. 
       puts "The line is cuttently: #{people_in_line}. #{other_deli}."
       break
